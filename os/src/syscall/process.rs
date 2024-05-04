8e65653a03cad818357a3511e9b87b69e03eef3a
Nguyen Dinh Phi <phind.uet@gmail.com>
2024-05-04 17:59:20 +0000

    phi.nguyendp: fix sys_get_time()

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,8 @@
+use core::time;
+
+use crate::mm::translated_byte_buffer;
 use crate::task::{
-    suspend_current_and_run_next,
-    exit_current_and_run_next,
+    current_user_token, exit_current_and_run_next, suspend_current_and_run_next
 };
 use crate::timer::get_time_us;
 
@@ -22,13 +24,20 @@ pub fn sys_yield() -> isize {
     0
 }
 
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    let _us = get_time_us();
-    // unsafe {
-    //     *ts = TimeVal {
-    //         sec: us / 1_000_000,
-    //         usec: us % 1_000_000,
-    //     };
-    // }
+// The address provided in parameter is user space address
+pub fn sys_get_time(ts: *mut TimeVal, tz: usize) -> isize {
+    let us = get_time_us();
+    let time_val_size = core::mem::size_of::<TimeVal>();
+    let mut buffers = translated_byte_buffer(current_user_token(), ts as *mut u8, time_val_size);
+    let tv = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    let tv_pointer = core::ptr::addr_of!(tv) as *const u8;
+    let tv_slice: &[u8]= unsafe {core::slice::from_raw_parts(tv_pointer, core::mem::size_of::<TimeVal>())};
+
+    buffers[0].copy_from_slice(tv_slice);
+
     0
 }
